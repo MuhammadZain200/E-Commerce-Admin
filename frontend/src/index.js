cbd8e5dce99f2b0a207a951608e660ccf1e38921
MuhammadZain200 <m.zain.a.malik1@gmail.com>
2026-01-26 17:08:21 +0500

    Commented Strict Mode so that it doesnt run apis more than once

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -9,7 +9,7 @@ import { NotificationProvider } from './context/NotificationContext';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
+  // <React.StrictMode>
     <AuthProvider>
       <CartProvider>
         <NotificationProvider>
@@ -17,7 +17,7 @@ root.render(
         </NotificationProvider>
       </CartProvider>
     </AuthProvider>
-  </React.StrictMode>
+  /* </React.StrictMode> */
 );
 
 // If you want to start measuring performance in your app, pass a function
